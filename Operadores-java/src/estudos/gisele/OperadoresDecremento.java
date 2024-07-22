803b84f102dfd10b6fbcebf5c5aee6872ed8ee10
Gisele Novaes <gisellemnovaes@hotmail.com>
2024-07-22 16:57:27 -0300

    Operadores Decremento

---
+++ Operadores-java/src/estudos/gisele/OperadoresDecremento.java
@@ -0,0 +1,20 @@
+package estudos.gisele;
+public class OperadoresDecremento {
+
+    public static void main(String[] args) {
+        int numero = 5;
+        //x repeticao
+    // numero = numero - 2;
+
+    //    numero --;
+    // numero = numero +1;    
+        
+     //   System.out.println(numero --); Imprime o número e depois o decremento
+
+        System.out.println(-- numero); //Imprime o decremento primeiro e depois o número já decrementado.
+        
+        System.out.println(numero);
+    }
+    
+}
+
